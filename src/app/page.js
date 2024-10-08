6d8fe65b9213cd4816bb6d055b515b1cf366e99a
abhimanyu <abhimanyu-sharma@csgroupchd.com>
2024-10-08 11:09:45 +0530

    remove error from site

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,13 @@ import bannerImage from "../assets/ProductImages/iphone.jpg"
 import shoes from "../assets/ProductImages/shoes1.jpg"
 import Cardhovers from "@/common/Cardhover"
 import HoverComponent from "@/common/Cardhover"
-import { ImageSlider } from "@/common/ImageSlider"
-import Slikslider from "@/common/Slikslider"
 
 
 export function Home() {
     return (
         <>
 
-            <section className="w-full">
-                <ImageSlider />
-            </section>
-
+          
 
             <section className="bg-black mt-10">
 
